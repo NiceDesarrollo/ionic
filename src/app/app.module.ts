ac147fc73b8096468edf7c80d01fc75cfa057d1d
Gabriel Ahumada <gahumada@nice.com.mx>
2024-02-16 17:59:22 -0600

    Vistas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"ionic-b9598","appId":"1:569054888168:web:744674142a87178198495d","storageBucket":"ionic-b9598.appspot.com","apiKey":"AIzaSyDF899u455CrLGO9x3mVk-yyVRPHUsX1mA","authDomain":"ionic-b9598.firebaseapp.com","messagingSenderId":"569054888168","measurementId":"G-SNZ1JX24Z4"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDF899u455CrLGO9x3mVk-yyVRPHUsX1mA",
+    authDomain: "ionic-b9598.firebaseapp.com",
+    projectId: "ionic-b9598",
+    storageBucket: "ionic-b9598.appspot.com",
+    messagingSenderId: "569054888168",
+    appId: "1:569054888168:web:744674142a87178198495d",
+    measurementId: "G-SNZ1JX24Z4"
+  }
 };
 
 /*
